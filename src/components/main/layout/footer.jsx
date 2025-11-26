fb495d3ec0e000749e47a7a22a2fee1040ce2c95
novilfahlevy <novilfreon@gmail.com>
2025-11-26 20:51:31 +0800

    fix footer

--- src/components/main/layout/footer.jsx
+++ src/components/main/layout/footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white mt-16">
       <div className="container mx-auto px-6 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="flex items-center space-x-2 mb-4 md:mb-0">
+          <div className="flex items-center space-x-2 mb-8 md:mb-0">
             <Link href="/" className="flex items-center space-x-2">
               <img
                 src="/img/LOGO1.png"
-                className="w-32 md:w-40 object-contain"
+                className="w-16 md:w-12 object-contain"
                 alt="Logo"
               />
             </Link>
